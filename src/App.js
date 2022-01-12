cef82faf456423503cb2c49e215439285f1601f8
amyeguo <72362221+amyeguo@users.noreply.github.com>
2022-01-12 17:22:32 -0600

    Connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 import EventList from "./components/EventList";
+import { useData } from "./utilities/firebase.js";
+
+function getEventList(events) {
+  return Object.entries(events);
+}
 
 function App() {
+  const [eventList, loading, error] = useData("/events", getEventList);
+  console.log("EVENT LIST");
+  console.log(eventList);
+
   return (
     <div className="App">
       <h1> UHangout</h1>
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,15 +1,52 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { useState, useEffect } from "react";
+import { getDatabase, onValue, ref, set } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7mpjxeDj2hYyFJ4JkXhctKL2lzjRb5tM",
-    authDomain: "uhangout-5cbf7.firebaseapp.com",
-    databaseURL: "https://uhangout-5cbf7-default-rtdb.firebaseio.com",
-    projectId: "uhangout-5cbf7",
-    storageBucket: "uhangout-5cbf7.appspot.com",
-    messagingSenderId: "435606287641",
-    appId: "1:435606287641:web:56be8346407796c3470188"
+  apiKey: "AIzaSyB7mpjxeDj2hYyFJ4JkXhctKL2lzjRb5tM",
+  authDomain: "uhangout-5cbf7.firebaseapp.com",
+  databaseURL: "https://uhangout-5cbf7-default-rtdb.firebaseio.com",
+  projectId: "uhangout-5cbf7",
+  storageBucket: "uhangout-5cbf7.appspot.com",
+  messagingSenderId: "435606287641",
+  appId: "1:435606287641:web:56be8346407796c3470188",
 };
 
 const firebase = initializeApp(firebaseConfig);
 const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const dbRef = ref(database, path);
+    const devMode =
+      !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+    if (devMode) {
+      console.log(`loading ${path}`);
+    }
+    return onValue(
+      dbRef,
+      (snapshot) => {
+        console.log("snapshot");
+        console.log(snapshot.val());
+        const val = snapshot.val();
+        if (devMode) {
+          console.log(val);
+        }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      },
+      (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      }
+    );
+  }, [path, transform]);
+
+  return [data, loading, error];
+};
