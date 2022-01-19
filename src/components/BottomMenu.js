e7c9e4acf42612f8b2947867a532078d936ff362
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2022-01-18 20:23:44 -0600

    createEvent

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -1,42 +1,47 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Event from "./Event";
 import { database } from "../utilities/firebase";
-import Paper from '@mui/material/Paper'
+import Paper from "@mui/material/Paper";
 import { makeStyles } from "@mui/styles";
-import BottomNavigation from '@mui/material/BottomNavigation'
-import BottomNavigationAction from '@mui/material/BottomNavigationAction'
-import AddIcon from '@mui/icons-material/Add'
-import ForumIcon from '@mui/icons-material/Forum'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { TextField } from '@mui/material'
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import AddIcon from "@mui/icons-material/Add";
+import ForumIcon from "@mui/icons-material/Forum";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { TextField } from "@mui/material";
 /* For "Create" Button in Modal Box */
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
 
 const useStyles = makeStyles({
-    container: {
-        background: 'white', 
-        width: '400px',
-        height: '500px'    
-    },
+  container: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    background: "white",
+    width: "350px",
+    height: "500px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
 });
-  
 
 const BottomMenu = () => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
-    const [name, setName] = useState("");
-    const [max, setMax] = useState("");
-    const [description, setDescription] = useState("");
-/*
+  const [name, setName] = useState("");
+  const [max, setMax] = useState("");
+  const [description, setDescription] = useState("");
+  /*
     const onCreate = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
     
@@ -53,24 +58,63 @@ const BottomMenu = () => {
     }
 */
 
-    return (
-        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-            <div>
-                <BottomNavigation showLabels>
-                    <BottomNavigationAction label="Add Event" icon={<AddIcon />} onClick={handleOpen}/>
-                    <BottomNavigationAction label="Messages" icon={<ForumIcon />} />
-                </BottomNavigation>
-                <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" sx={{'& .MuiTextField-root': { m: 2, width: '25ch' }, }}>
-                    <Box className={classes.container}>
-                        <TextField required value={name} onChange = {(event) => setName(event.target.value)} helperText="Please enter event name" label="Event Name" variant="outlined" />
-                        <TextField value={max} onChange = {(event) => setMax(event.target.value)} label="No. of People" type="number" InputLabelProps={{shrink: true,}} />
-                        <TextField value={description} onChange = {(event) => setDescription(event.target.value)} label="Description" multiline rows={4} />
-                        <Button variant="contained" endIcon={<SendIcon />} onClick={console.log("Create Event")}>   Create   </Button>
-                    </Box>
-                </Modal>
-            </div>
-        </Paper>
-    );
-}
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <div>
+        <BottomNavigation showLabels>
+          <BottomNavigationAction
+            label="Add Event"
+            icon={<AddIcon />}
+            onClick={handleOpen}
+          />
+          <BottomNavigationAction label="Messages" icon={<ForumIcon />} />
+        </BottomNavigation>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          sx={{ "& .MuiTextField-root": { m: 2, width: "25ch" } }}
+        >
+          <Box className={classes.container}>
+            <TextField
+              required
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              helperText="Please enter event name"
+              label="Event Name"
+              variant="outlined"
+            />
+            <TextField
+              value={max}
+              onChange={(event) => setMax(event.target.value)}
+              label="No. of People"
+              type="number"
+              InputLabelProps={{ shrink: true }}
+            />
+            <TextField
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
+              label="Description"
+              multiline
+              rows={4}
+            />
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={console.log("Create Event")}
+            >
+              {" "}
+              Create{" "}
+            </Button>
+          </Box>
+        </Modal>
+      </div>
+    </Paper>
+  );
+};
 
-export default BottomMenu
+export default BottomMenu;
