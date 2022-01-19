8586f225a49b6ba784569532f6e0fa9f7eda88a4
amyeguo <72362221+amyeguo@users.noreply.github.com>
2022-01-18 20:43:53 -0600

    Reformat the event card, prevent joining after reaching max capacity

--- src/components/Event.js
+++ src/components/Event.js
@@ -6,29 +6,46 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { purple } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
 import { pushData } from "../utilities/firebase";
 
-export default function Event({ event, clickedEvent, setclickedEvent}) {
+export default function Event({ event, clickedEvent, setclickedEvent }) {
+  console.log("POSIITON OF INTERSSST");
   console.log(event);
+  const currCapacity = Object.keys(event.people).length;
   return (
     <Card sx={{ maxWidth: 345, mb: 3 }}>
-      <CardHeader title={event.title} subheader={event.date}></CardHeader>
+      <CardHeader title={event.name} subheader={event.date}></CardHeader>
       <CardMedia
         component="img"
         height="140"
-        image="https://nurecreation.com/images/2015/8/16//SailingCenter.jpg?width=600&height=360&mode=crop"
+        image={event.photoUrl}
         alt="South Beach"
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div"></Typography>
+        <Typography gutterBottom variant="body" component="div">
+          Time: {event.time}
+        </Typography>
+        <Typography gutterBottom variant="body" component="div">
+          Capacity: {currCapacity} / {event.max}
+        </Typography>
         <Typography variant="body2" color="text.secondary">
-          Description: {event.description}
+          {event.description}
         </Typography>
       </CardContent>
-      <CardActions style={{justifyContent: 'center'}}>
-        <JoinButton key={event} event = {event} clickedEvent={clickedEvent} setclickedEvent={setclickedEvent} checked={event === clickedEvent} />
+      <CardActions style={{ justifyContent: "center" }}>
+        {currCapacity >= event.max ? (
+          <Button disabled> Event Full </Button>
+        ) : (
+          <JoinButton
+            key={event}
+            event={event}
+            clickedEvent={clickedEvent}
+            setclickedEvent={setclickedEvent}
+            checked={event === clickedEvent}
+          />
+        )}
       </CardActions>
     </Card>
   );
@@ -41,55 +58,30 @@ const theme = createTheme({
     },
     secondary: {
       // This is green.A700 as hex.
-      main: '#11cb5f',
+      main: "#11cb5f",
     },
   },
 });
 
-// const JoinButton = ({event, clickedEvent,setclickedEvent,checked}) =>(
-//   <>
-    
-//     {/* <input type="radio" id={clickedEvent} className="btn-check" checked={checked} autoComplete="off"
-//       onChange={() => setclickedEvent(clickedEvent)} /> */}
-
-//     <ThemeProvider theme={theme}>
-//     <Button onClick={()=>updatePeopleData(clickedEvent)} 
-//       variant="contained" color='secondary' htmlFor={clickedEvent}>
-//     Join Event
-//     </Button>
-//     </ThemeProvider>
-//   </>
-  
 // );
 
-const JoinButton = ({event,clickedEvent,setclickedEvent,checked}) =>{
-  function updatePeopleData(event){
-    console.log("position of interest")
-    console.log(event);
-    pushData('events/'+ event.id + '/people',"newPerson");
+const JoinButton = ({ event, clickedEvent, setclickedEvent, checked }) => {
+  function updatePeopleData(event) {
+    pushData("events/" + event.id + "/people", "newPerson");
   }
 
-  return(
-    
-    // {/* <input type="radio" id={clickedEvent} className="btn-check" checked={checked} autoComplete="off"
-    //   onChange={() => setclickedEvent(clickedEvent)} /> */}
+  return (
     <>
-    <ThemeProvider theme={theme}>
-    <Button onClick={()=>updatePeopleData(event)} variant="contained" color='secondary' htmlFor={clickedEvent}>
-    Join Event
-    </Button>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Button
+          onClick={() => updatePeopleData(event)}
+          variant="contained"
+          color="secondary"
+          htmlFor={clickedEvent}
+        >
+          Join Event
+        </Button>
+      </ThemeProvider>
     </>
-  )
+  );
 };
-
-
-
-// const Event = ({ data }) => {
-//     return (
-//       <div style={{ border: "1px solid black" }}>
-//         {" "}
-//         <h3>{data[0].title}</h3>
-//       </div>
-//     );
-//   };
