bd1c47b95d0ee9ad63c1eabe99f9b2b45a8adac8
collinmurch <collin@murch.net>
2022-01-28 14:27:51 -0600

    Fixed uploaded image not displaying bug

--- src/components/Event.js
+++ src/components/Event.js
@@ -28,7 +28,7 @@ const isUIDinJoinedMembers = (uid, joinedMembers) => {
 export default function Event({ event, userList, user }) {
   const currCapacity = Object.keys(event.people).length;
   let [joined, setJoined] = useState(false); // handle can't-join-twice later
-
+  
   return (
     <Card sx={{ maxWidth: 345, mb: 5 }}>
       <CardHeader
@@ -38,7 +38,7 @@ export default function Event({ event, userList, user }) {
           getUserFromUID(event.people[0], userList).displayName
         }`}
       ></CardHeader>
-      <CardMedia component="img" imageURL={event.photoURL} height="140" image="filler" alt={event.name} />
+      <CardMedia component="img" imageURL={event.photoUrl} height="140" image={event.photoUrl} alt={event.name} />
       <CardContent>
         <Typography gutterBottom variant="body" component="div">
           Time: {moment(event.eventTime).format("MMMM Do YYYY, h:mm a")}
