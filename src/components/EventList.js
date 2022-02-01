177ecdf1a5b2b87c67b97f76dde2fb568fc4fed2
amyeguo <72362221+amyeguo@users.noreply.github.com>
2022-01-31 19:47:24 -0600

    Fixed error where events weren't displaying when logging out

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,20 +3,22 @@ import Event from "./Event";
 import Box from "@mui/material/Box";
 
 const EventList = ({ events, userList, user }) => {
-  const userId = user ? user.uid : ""
+  const userId = user ? user.uid : "";
 
   return (
     <Box sx={{ mx: "auto", width: 300 }}>
-      {events.map((event) => { 
-
-        return !Object.values(event.people).includes(userId) && (
-        <Event
-          key={event.id}
-          event={event}
-          userList={userList}
-          user={user}
-        />
-      )})}
+      {events.map((event) => {
+        return (
+          !Object.values(event.people).includes(userId) && (
+            <Event
+              key={event.id}
+              event={event}
+              userList={userList}
+              user={userId}
+            />
+          )
+        );
+      })}
     </Box>
   );
 };
