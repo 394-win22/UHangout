db77bb7bc7f5f1c9e81e0990c58409d1fa733471
collinmurch <collin@murch.net>
2022-01-31 18:28:07 -0600

    Changed background and navbar color

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -10,7 +10,7 @@ import SignOutButton from './SignOutButton'
 const TopNavBar = ({ isLoggedIn, setQuery }) => {
   return (
     <Box sx={{ flexGrow: 1, paddingBottom: 3 }}>
-      <AppBar position="static">
+      <AppBar position="static" style={{ background: '#465a82' }}>
         <Toolbar>
           <Typography variant="h5" component="div" sx={{ flexGrow: 1, paddingLeft: 1 }}>
             UHangout
