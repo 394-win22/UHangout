0e15ebb42409b2bf5deb70d985e7f4be564a6664
collinmurch <collin@murch.net>
2022-02-02 19:29:41 -0600

    Working filter by time

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -38,7 +38,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
-      width: '20ch',
+      width: '40ch',
     },
   },
 }));
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,20 +6,59 @@ import { getUserFromUID } from "./Event"
 
 import TopNavBar from "./TopNavBar"
 
+const parseTime = input => {
+   let parsedTime = moment(input, [
+    'h',
+    'h a',
+    'h:mm',
+    'h:mm a',
+    'MMMM',
+    'MMMM D',
+    'MMMM Do',
+    'MMMM Do h',
+    'MMMM Do h:mm',
+    'MMMM Do h:mm a'
+  ])
+
+  if (parsedTime.format() != 'Invalid date')
+    return parsedTime
+  else return false
+}
+
+const matchTime = (parsedTime, duration, eventTime, input) => {
+    const parsedDuration = parseInt(duration)
+    if (!isNaN(parsedDuration)) {
+      const endTime = eventTime.add(duration, 'h').format('YYYY-MM-DD HH:mm')
+      eventTime.subtract(duration, 'h')
+
+      if (parsedTime.isBetween(eventTime, endTime))
+        return true
+    }
+
+    if (eventTime.format('MMMM Do') == parsedTime.format('MMMM Do')) 
+      return true
+
+    // Note: will not match if only month is supplied (auto-completes with today's date)
+    // TODO: Make so if only month is put in, then return all events in that month
+    return eventTime.format('MMM').toLowerCase() == input || eventTime.format('MMMM').toLowerCase() == input
+}
+
 export const Welcome = ({ user, events, userList }) => {
   const [query, setQuery] = useState("");
 
   let filteredEvents = events;
   if (query != "") {
     const lowerCaseQuery = query.toLowerCase()
+    const parsedTime = parseTime(lowerCaseQuery)
+  
     filteredEvents = events.filter((e) => {
-      let time = moment(e.eventTime)
-      time = time.format('MMMM Do YYYY, h:mm a')
+      console.log(e)
+      let eventTime = moment(e.eventTime)
 
       return e.name.toLowerCase().includes(lowerCaseQuery)
         || e.description.toLowerCase().includes(lowerCaseQuery)
         || getUserFromUID(Object.values(e.people)[0], userList).displayName.toLowerCase().includes(lowerCaseQuery)
-        || time.toLowerCase().includes(lowerCaseQuery)
+        || (parsedTime && matchTime(parsedTime, e.duration, eventTime, lowerCaseQuery))
     });
   }
 
